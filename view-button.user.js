1c355af0df68b41d30a85f7cbcf92bfd87390528
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2018-02-17 20:28:13 +0200

    Fixed domains rule

--- view-button.user.js
+++ view-button.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         View Button
 // @namespace    view-button
-// @version      0.1
+// @version      0.1.1
 // @description  Returns back "View Image" button for google images
 // @author       coffecode
-// @include      /^https://www.google.(com(.*))/(imgres|search)*
+// @include      /^https://(.*).google.([a-z\.]*)/(imgres|search)*
 // @downloadURL  https://raw.githubusercontent.com/0xC0FFEEC0DE/make-view-button-back-again/master/view-button.user.js
 // @updateURL    https://raw.githubusercontent.com/0xC0FFEEC0DE/make-view-button-back-again/master/view-button.user.js
 // @grant        none
