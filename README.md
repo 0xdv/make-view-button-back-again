33d3dc5efabb63a849aefe5bb3f5d1f08428e831
0xC0FFEEC0DE <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2018-02-17 16:43:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# make-view-button-back-again
+Returns back "View Image" button for google images
+
+# Installation
+Requires [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?utm_source=chrome-ntp-icon)!
+
+click -> https://raw.githubusercontent.com/0xC0FFEEC0DE/make-view-button-back-again/master/view-button.user.js
---
+++ view-button.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         View Button
+// @namespace    view-button
+// @version      0.1
+// @description  Returns back "View Image" button for google images
+// @author       coffecode
+// @include      /^https://www.google.(com(.*))/(imgres|search)*
+// @downloadURL  https://raw.githubusercontent.com/0xC0FFEEC0DE/make-view-button-back-again/master/view-button.user.js
+// @updateURL    https://raw.githubusercontent.com/0xC0FFEEC0DE/make-view-button-back-again/master/view-button.user.js
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("start");
+
+    let buttonClass = "view_button";
+
+    let imageObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            //console.log(mutation);
+            if(mutation.target.src !== "" && mutation.target.classList.contains('irc_mi')) {
+                let container = mutation.target.closest('.irc_c');
+
+                let span = document.createElement('span');
+                span.textContent = 'View Image';
+
+                let btn = document.createElement('a');
+                btn.className += buttonClass;
+                btn.target = '_blank';
+                btn.href = mutation.target.src;
+                btn.appendChild(span);
+
+                let td = document.createElement('td');
+                td.appendChild(btn);
+
+                let menu = container.querySelector('table.irc_but_r tbody tr');
+                let existBtn = menu.querySelector("."+buttonClass);
+                if(existBtn) {
+                    existBtn.parentNode.removeChild(existBtn);
+                }
+
+                menu.insertAdjacentElement('afterbegin', td);
+
+            }
+        });
+    });
+
+    imageObserver.observe(document.body, {
+        attributes: true,
+        subtree: true,
+        attributeFilter: ['src']
+    });
+
+})();
